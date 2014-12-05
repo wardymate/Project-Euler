be6d5d3e1893b25af7be9dad449d568bc69c4eef
Chris Ward <chriswarddev@gmail.com>
2014-12-05 14:00:30 +0000

    problem 4 solved

---
+++ problem4.rb
@@ -0,0 +1,14 @@
+arr = []
+$y=999
+x=999
+while $y>100 do
+	999.downto(100) do |x|
+	num = x*$y
+	puts "#{x} * #{$y} = #{num}"
+		if num.to_s == num.to_s.reverse
+		arr << num
+		end
+	end
+	$y=$y-1
+end
+puts arr.sort.last
