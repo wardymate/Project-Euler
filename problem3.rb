0ad97093529aa92972373cdcd301b205cb1f5754
Chris Ward <chriswarddev@gmail.com>
2014-12-05 01:20:43 +0000

    problem 3 solved

--- problem3.rb
+++ problem3.rb
@@ -1,26 +1,16 @@
-#The prime factors of 13195 are 5, 7, 13 and 29.
-
-#What is the largest prime factor of the number 600851475143
-def is_prime a
-  for d in 2..(a - 1)
-   if (a % d) == 0
-    return false
-   end
-  end
+y = 2
+num = 600851475143
+arr = []
+until num == 1 do
+		if num % y == 0
+		arr << y
+		num = num/y
+		y = y+1
+		else
+		y = y+1
+		end
+	
 end
-
-
-def solution(n)
-arr =[]
-(1..n).each do |x| 
-    if n % x ==0
-    if x.is_prime
-    	arr << x
-    	end 
-end
-end
+arr << num
 puts arr
-end
 
-
-solution (13195)
