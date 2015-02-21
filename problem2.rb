30757df6a5017a9a0a5d43446e187b8f31381294
Chris Ward <chriswarddev@gmail.com>
2015-02-21 22:16:26 +0000

    problem2 updated

--- problem2.rb
+++ problem2.rb
@@ -4,13 +4,14 @@ y = 2
 z = 0
 array = [1,2]
 while x < 4000000
-z = x+y
-array << z
-x=y
-y=z
+  z = x+y
+  array << z
+  x=y
+  y=z
 end
+
 array2 = array.pop(2)
 # puts array check we have the fobinoachi array
 array3 = array.select {|number| number % 2 == 0}
 solution = array3.inject(:+)
-puts solution 
+puts solution
